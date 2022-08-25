6eb0b1916707e1745697836d222632e381dd7e2a
Patrick10839087 <pkdogbatse+github@st.ug.edu.gh>
2022-08-25 14:49:27 +0000

    form1 name changed to Login

--- InventoryShopRite/Login.Designer.cs
+++ InventoryShopRite/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InventoryShopRite
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
