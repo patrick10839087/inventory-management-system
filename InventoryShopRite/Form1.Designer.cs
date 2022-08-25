c48eb4d0a9ea9ad9f45a6565a4fbb80e25b6f8f3
Patrick10839087 <pkdogbatse+github@st.ug.edu.gh>
2022-08-25 14:38:13 +0000

    checkbox to show password

--- InventoryShopRite/Form1.Designer.cs
+++ InventoryShopRite/Form1.Designer.cs
@@ -28,16 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.passwordTab = new System.Windows.Forms.TextBox();
+            this.userIDTab = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // splitter1
@@ -52,16 +56,93 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.passwordTab);
+            this.panel1.Controls.Add(this.userIDTab);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(624, 188);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(524, 444);
             this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(162, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(323, 366);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 24);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Show Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(254, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(94, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(94, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Password";
+            // 
+            // passwordTab
+            // 
+            this.passwordTab.BackColor = System.Drawing.SystemColors.Window;
+            this.passwordTab.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.passwordTab.Location = new System.Drawing.Point(215, 246);
+            this.passwordTab.Name = "passwordTab";
+            this.passwordTab.PasswordChar = '*';
+            this.passwordTab.Size = new System.Drawing.Size(209, 27);
+            this.passwordTab.TabIndex = 3;
+            this.passwordTab.Text = "Password";
+            this.passwordTab.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // userIDTab
+            // 
+            this.userIDTab.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.userIDTab.Location = new System.Drawing.Point(215, 179);
+            this.userIDTab.Name = "userIDTab";
+            this.userIDTab.Size = new System.Drawing.Size(209, 27);
+            this.userIDTab.TabIndex = 2;
+            this.userIDTab.Text = "User ID";
             // 
             // label1
             // 
@@ -84,53 +165,6 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Inventory Management System";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(162, 136);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(209, 27);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox2.Location = new System.Drawing.Point(162, 203);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(209, 27);
-            this.textBox2.TabIndex = 3;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(41, 203);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 28);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Password";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(41, 136);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(31, 28);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "ID";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.ForeColor = System.Drawing.SystemColors.Info;
-            this.button1.Location = new System.Drawing.Point(201, 262);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 36);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -147,6 +181,7 @@
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,8 +195,10 @@
         private Button button1;
         private Label label5;
         private Label label4;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox passwordTab;
+        private TextBox userIDTab;
         private Label label2;
+        private PictureBox pictureBox1;
+        private CheckBox checkBox1;
     }
 }
--- InventoryShopRite/Form1.cs
+++ InventoryShopRite/Form1.cs
@@ -11,5 +11,20 @@ namespace InventoryShopRite
         {
 
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            passwordTab.PasswordChar = checkBox1.Checked ? '\0' : '*';
+        }
     }
 }
