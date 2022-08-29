7ab6f21a32d4c79fc858ba33d2eeff2ffb96fe47
Patrick10839087 <pkdogbatse+github@st.ug.edu.gh>
2022-08-29 15:34:39 +0000

    sales carting

--- InventoryShopRite/Sales.Designer.cs
+++ InventoryShopRite/Sales.Designer.cs
@@ -34,31 +34,33 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.comboSearch = new System.Windows.Forms.ComboBox();
+            this.userShow1 = new System.Windows.Forms.DataGridView();
+            this.quantityBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TotAmount = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.userShow)).BeginInit();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userShow1)).BeginInit();
             this.SuspendLayout();
             // 
             // userShow
             // 
             this.userShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.userShow.Location = new System.Drawing.Point(142, 130);
+            this.userShow.Location = new System.Drawing.Point(12, 130);
             this.userShow.Name = "userShow";
             this.userShow.ReadOnly = true;
             this.userShow.RowHeadersWidth = 51;
             this.userShow.RowTemplate.Height = 29;
-            this.userShow.Size = new System.Drawing.Size(517, 321);
+            this.userShow.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.userShow.Size = new System.Drawing.Size(628, 321);
             this.userShow.TabIndex = 1;
             this.userShow.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.userShow_CellContentClick);
             // 
@@ -68,16 +70,17 @@
             this.panel3.Controls.Add(this.pictureBox2);
             this.panel3.Controls.Add(this.label6);
             this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.pictureBox1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(125, 0);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1212, 53);
+            this.panel3.Size = new System.Drawing.Size(1337, 53);
             this.panel3.TabIndex = 4;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1156, 0);
+            this.pictureBox2.Location = new System.Drawing.Point(1281, 0);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(56, 53);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -90,7 +93,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label6.ForeColor = System.Drawing.Color.DarkRed;
-            this.label6.Location = new System.Drawing.Point(486, 9);
+            this.label6.Location = new System.Drawing.Point(630, 9);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(79, 37);
             this.label6.TabIndex = 2;
@@ -101,61 +104,11 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label5.ForeColor = System.Drawing.Color.Purple;
-            this.label5.Location = new System.Drawing.Point(6, 9);
+            this.label5.Location = new System.Drawing.Point(131, 9);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(180, 28);
             this.label5.TabIndex = 1;
             this.label5.Text = "ShopRite Inventory";
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Orange;
-            this.panel1.Controls.Add(this.button7);
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(125, 821);
-            this.panel1.TabIndex = 3;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(12, 398);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(94, 29);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "Categories";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(12, 222);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 29);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Dashboard";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(12, 282);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Users";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 347);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Products";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -169,7 +122,7 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(409, 97);
+            this.dateTimePicker1.Location = new System.Drawing.Point(390, 97);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
             this.dateTimePicker1.TabIndex = 4;
@@ -179,7 +132,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(409, 63);
+            this.label1.Location = new System.Drawing.Point(390, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(110, 31);
             this.label1.TabIndex = 4;
@@ -188,22 +141,90 @@
             // comboSearch
             // 
             this.comboSearch.FormattingEnabled = true;
-            this.comboSearch.Location = new System.Drawing.Point(142, 96);
+            this.comboSearch.Location = new System.Drawing.Point(12, 96);
             this.comboSearch.Name = "comboSearch";
             this.comboSearch.Size = new System.Drawing.Size(174, 28);
             this.comboSearch.TabIndex = 39;
             this.comboSearch.Text = "Select Category";
+            this.comboSearch.SelectedIndexChanged += new System.EventHandler(this.comboSearch_SelectedIndexChanged);
+            // 
+            // userShow1
+            // 
+            this.userShow1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.userShow1.Location = new System.Drawing.Point(697, 130);
+            this.userShow1.Name = "userShow1";
+            this.userShow1.ReadOnly = true;
+            this.userShow1.RowHeadersWidth = 51;
+            this.userShow1.RowTemplate.Height = 29;
+            this.userShow1.Size = new System.Drawing.Size(628, 321);
+            this.userShow1.TabIndex = 40;
+            this.userShow1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.userShow1_CellContentClick);
+            // 
+            // quantityBox
+            // 
+            this.quantityBox.Location = new System.Drawing.Point(806, 99);
+            this.quantityBox.Name = "quantityBox";
+            this.quantityBox.Size = new System.Drawing.Size(125, 27);
+            this.quantityBox.TabIndex = 41;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(698, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 31);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Quantity";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(937, 99);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 29);
+            this.button6.TabIndex = 43;
+            this.button6.Text = "Add To Order";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(747, 483);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 31);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Total Amount";
+            // 
+            // TotAmount
+            // 
+            this.TotAmount.AutoSize = true;
+            this.TotAmount.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TotAmount.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.TotAmount.Location = new System.Drawing.Point(904, 483);
+            this.TotAmount.Name = "TotAmount";
+            this.TotAmount.Size = new System.Drawing.Size(61, 31);
+            this.TotAmount.TabIndex = 45;
+            this.TotAmount.Text = "GHS";
             // 
             // Sales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1337, 821);
+            this.Controls.Add(this.TotAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.quantityBox);
+            this.Controls.Add(this.userShow1);
             this.Controls.Add(this.comboSearch);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.userShow);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Sales";
@@ -213,8 +234,8 @@
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userShow1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,14 +248,15 @@
         private PictureBox pictureBox2;
         private Label label6;
         private Label label5;
-        private Panel panel1;
-        private Button button7;
-        private Button button4;
-        private Button button3;
-        private Button button2;
         private PictureBox pictureBox1;
         private DateTimePicker dateTimePicker1;
         private Label label1;
         private ComboBox comboSearch;
+        private DataGridView userShow1;
+        private TextBox quantityBox;
+        private Label label2;
+        private Button button6;
+        private Label label3;
+        private Label TotAmount;
     }
 }
--- InventoryShopRite/Sales.cs
+++ InventoryShopRite/Sales.cs
@@ -18,6 +18,7 @@ namespace InventoryShopRite
             InitializeComponent();
             curData();
         }
+        DataTable table = new DataTable();
         // establishing database connection
         public static string connectionString = "server=localhost;database=inventory-management-system;uid=root; pwd=\"\";";
         public static MySqlConnection connection = new MySqlConnection(connectionString);
@@ -29,6 +30,9 @@ namespace InventoryShopRite
 
             userShow.DataSource = dTable;
         }
+        int num = 0;
+        string product;
+        int quantity, uPrice, totPrice;
         void comboFill()
         {
             string query = "SELECT * FROM categories";
@@ -53,7 +57,48 @@ namespace InventoryShopRite
 
             }
         }
+        int flag = 0;
         private void userShow_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.userShow.Rows[e.RowIndex];
+                //num = Convert.ToInt32(row.Cells[0].Value.ToString());
+                product = row.Cells[1].Value.ToString();
+                //quantity = Convert.ToInt32(quantityBox.Text);
+                uPrice = Convert.ToInt32(row.Cells[3].Value.ToString());
+                //totPrice = quantity * uPrice;
+                flag = 1;
+            }
+        }
+
+        private void userShow1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataTable dTable = new DataTable();
+
+            userShow1.DataSource = dTable;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            if (quantityBox.Text == "")
+                MessageBox.Show("ENTER THE NUMBER OF PRODUCTS");
+            else if (flag == 0)
+                MessageBox.Show("SELECT THE PRODUCT");
+            else
+            {
+                num += 1;
+                quantity = Convert.ToInt32(quantityBox.Text);
+                totPrice = quantity * uPrice;
+                table.Rows.Add(num, product, quantity, uPrice, totPrice);
+                userShow1.DataSource = table;
+                flag = 0;
+            }
+            sum = sum + totPrice;
+            TotAmount.Text = "GHS " + sum.ToString();
+        }
+
+        private void comboSearch_SelectedIndexChanged(object sender, EventArgs e)
         {
             MySqlDataAdapter adapterInstance = new MySqlDataAdapter("SELECT * FROM products WHERE proCategory = '" + comboSearch.SelectedValue.ToString() + "'", connection);
             DataTable dTable = new DataTable();
@@ -70,6 +115,14 @@ namespace InventoryShopRite
         private void Sales_Load(object sender, EventArgs e)
         {
             comboFill();
+
+            table.Columns.Add("Num", typeof(int));
+            table.Columns.Add("Product", typeof(string));
+            table.Columns.Add("Quantity", typeof(int));
+            table.Columns.Add("Unit Price", typeof(int));
+            table.Columns.Add("Total Price", typeof(int));
+
+            userShow1.DataSource = table;
         }
     }
 }
